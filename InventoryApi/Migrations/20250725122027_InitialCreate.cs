335899f4d6d2546010374b12ea5635368e7c3b78
Hamza Taşbay <T_4517-Hamza-4518@outlook.com>
2025-07-25 18:31:12 +0300

    Veritabanı scriptleri, migration dosyaları ve .gitignore güncellendi

---
+++ InventoryApi/Migrations/20250725122027_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "DeletedProducts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "DeletedProducts",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
